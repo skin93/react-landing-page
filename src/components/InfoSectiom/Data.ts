1d85bfba238030ec459e3e190717649da11a3333
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-08-03 22:25:17 +0200

    Update data

--- src/components/InfoSectiom/Data.ts
+++ src/components/InfoSectiom/Data.ts
@@ -1,4 +1,6 @@
-import Car from '../../images/car-1.svg';
+import Car from '../../images/car.svg';
+import Pig from '../../images/pig.svg';
+import Information from '../../images/information.svg';
 
 export const homeObjOne = {
   id: 'about',
@@ -16,4 +18,93 @@ export const homeObjOne = {
   dark: true,
   primary: true,
   darkText: false,
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: true,
+  topLine: 'Unlimited Access',
+  headline: 'Unlimited Transactions with zero fees',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees',
+  buttonLabel: 'Learn More',
+  imgStart: true,
+  img: Pig,
+  alt: 'Piggybank',
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join Our Team',
+  headline: 'Creating an account is extremely easy',
+  description:
+    "Get everythig set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go",
+  buttonLabel: 'Starting Now',
+  imgStart: false,
+  img: Information,
+  alt: 'Paper',
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+export const homeObjFour = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join Our Team',
+  headline: 'Creating an account is extremely easy',
+  description:
+    "Get everythig set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go",
+  buttonLabel: 'Starting Now',
+  imgStart: false,
+  img: Information,
+  alt: 'Paper',
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjFive = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join Our Team',
+  headline: 'Creating an account is extremely easy',
+  description:
+    "Get everythig set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go",
+  buttonLabel: 'Starting Now',
+  imgStart: false,
+  img: Information,
+  alt: 'Paper',
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjSix = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join Our Team',
+  headline: 'Creating an account is extremely easy',
+  description:
+    "Get everythig set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go",
+  buttonLabel: 'Starting Now',
+  imgStart: false,
+  img: Information,
+  alt: 'Paper',
+  dark: false,
+  primary: false,
+  darkText: true,
 };
