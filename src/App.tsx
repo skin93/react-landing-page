e7a7e9ff12e61ef80c1e4a055647f0fc17f6ee08
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-08-04 10:18:51 +0200

    Navigate to SignIn page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignInPage from './pages/SignIn';
+import HomePage from './pages/Home';
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Switch>
+        <Route path='/' component={HomePage} exact />
+        <Route path='/signin' component={SignInPage} exact />
+      </Switch>
     </Router>
   );
 }
